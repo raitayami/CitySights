b96788224133cf87add5138df79c4488ca404e41
raitayami <66219215+raitayami@users.noreply.github.com>
2023-12-06 10:59:18 +0000

    - Add button and text field that takes the user's query

--- CitySights/ContentView.swift
+++ CitySights/ContentView.swift
@@ -8,12 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var query: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            
+            TextField("Where do you want to go?", text: $query)
+                .textFieldStyle(.roundedBorder)
+            
+            Button {
+                
+            } label: {
+                Text("Go")
+                    .padding(10)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    
+            }
+
         }
         .padding()
     }
