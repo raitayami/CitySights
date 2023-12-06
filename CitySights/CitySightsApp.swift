ee47aa0a52ec76d7e2ad0bb8209cbaea9c58a85e
raitayami <66219215+raitayami@users.noreply.github.com>
2023-12-06 10:31:23 +0000

    Initial Commit

---
+++ CitySights/CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsApp.swift
+//  CitySights
+//
+//  Created by Tayami Rai on 06/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
